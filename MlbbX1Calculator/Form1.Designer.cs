91b25874b9710d3d63c1ce463311c8bde365a0ab
Eric Monteiro Galotti de Souza <ericmgsouza@gmail.com>
2020-06-10 21:14:46 -0300

    Add files via upload

---
+++ MlbbX1Calculator/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace MlbbX1Calculator
+{
+	partial class frmPrincipal
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cmbHeroi1 = new System.Windows.Forms.ComboBox();
+			this.cmbHeroi2 = new System.Windows.Forms.ComboBox();
+			this.cmbItem1Heroi1 = new System.Windows.Forms.ComboBox();
+			this.cmbItem2Heroi1 = new System.Windows.Forms.ComboBox();
+			this.cmbItem6Heroi1 = new System.Windows.Forms.ComboBox();
+			this.cmbItem3Heroi1 = new System.Windows.Forms.ComboBox();
+			this.cmbItem5Heroi1 = new System.Windows.Forms.ComboBox();
+			this.cmbItem4Heroi1 = new System.Windows.Forms.ComboBox();
+			this.cmbItem4Heroi2 = new System.Windows.Forms.ComboBox();
+			this.cmbItem5Heroi2 = new System.Windows.Forms.ComboBox();
+			this.cmbItem3Heroi2 = new System.Windows.Forms.ComboBox();
+			this.cmbItem6Heroi2 = new System.Windows.Forms.ComboBox();
+			this.cmbItem2Heroi2 = new System.Windows.Forms.ComboBox();
+			this.cmbItem1Heroi2 = new System.Windows.Forms.ComboBox();
+			this.btnX1 = new System.Windows.Forms.Button();
+			this.lblHeroi1 = new System.Windows.Forms.Label();
+			this.lblHeroi2 = new System.Windows.Forms.Label();
+			this.lblItens1 = new System.Windows.Forms.Label();
+			this.lblItens2 = new System.Windows.Forms.Label();
+			this.imgHeroi1 = new System.Windows.Forms.PictureBox();
+			this.imgHeroi2 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.imgHeroi1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgHeroi2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// cmbHeroi1
+			// 
+			this.cmbHeroi1.BackColor = System.Drawing.SystemColors.Window;
+			this.cmbHeroi1.Cursor = System.Windows.Forms.Cursors.Default;
+			this.cmbHeroi1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbHeroi1.FormattingEnabled = true;
+			this.cmbHeroi1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+			this.cmbHeroi1.Location = new System.Drawing.Point(40, 214);
+			this.cmbHeroi1.Name = "cmbHeroi1";
+			this.cmbHeroi1.Size = new System.Drawing.Size(121, 21);
+			this.cmbHeroi1.TabIndex = 0;
+			this.cmbHeroi1.Tag = "";
+			this.cmbHeroi1.SelectedIndexChanged += new System.EventHandler(this.cmbHeroi1_SelectedIndexChanged);
+			// 
+			// cmbHeroi2
+			// 
+			this.cmbHeroi2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbHeroi2.FormattingEnabled = true;
+			this.cmbHeroi2.Location = new System.Drawing.Point(350, 212);
+			this.cmbHeroi2.Name = "cmbHeroi2";
+			this.cmbHeroi2.Size = new System.Drawing.Size(121, 21);
+			this.cmbHeroi2.TabIndex = 1;
+			this.cmbHeroi2.SelectedIndexChanged += new System.EventHandler(this.cmbHeroi2_SelectedIndexChanged);
+			// 
+			// cmbItem1Heroi1
+			// 
+			this.cmbItem1Heroi1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem1Heroi1.FormattingEnabled = true;
+			this.cmbItem1Heroi1.Location = new System.Drawing.Point(40, 265);
+			this.cmbItem1Heroi1.Name = "cmbItem1Heroi1";
+			this.cmbItem1Heroi1.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem1Heroi1.TabIndex = 2;
+			// 
+			// cmbItem2Heroi1
+			// 
+			this.cmbItem2Heroi1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem2Heroi1.FormattingEnabled = true;
+			this.cmbItem2Heroi1.Location = new System.Drawing.Point(40, 292);
+			this.cmbItem2Heroi1.Name = "cmbItem2Heroi1";
+			this.cmbItem2Heroi1.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem2Heroi1.TabIndex = 3;
+			// 
+			// cmbItem6Heroi1
+			// 
+			this.cmbItem6Heroi1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem6Heroi1.FormattingEnabled = true;
+			this.cmbItem6Heroi1.Location = new System.Drawing.Point(40, 400);
+			this.cmbItem6Heroi1.Name = "cmbItem6Heroi1";
+			this.cmbItem6Heroi1.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem6Heroi1.TabIndex = 4;
+			// 
+			// cmbItem3Heroi1
+			// 
+			this.cmbItem3Heroi1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem3Heroi1.FormattingEnabled = true;
+			this.cmbItem3Heroi1.Location = new System.Drawing.Point(40, 319);
+			this.cmbItem3Heroi1.Name = "cmbItem3Heroi1";
+			this.cmbItem3Heroi1.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem3Heroi1.TabIndex = 4;
+			// 
+			// cmbItem5Heroi1
+			// 
+			this.cmbItem5Heroi1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem5Heroi1.FormattingEnabled = true;
+			this.cmbItem5Heroi1.Location = new System.Drawing.Point(40, 373);
+			this.cmbItem5Heroi1.Name = "cmbItem5Heroi1";
+			this.cmbItem5Heroi1.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem5Heroi1.TabIndex = 5;
+			// 
+			// cmbItem4Heroi1
+			// 
+			this.cmbItem4Heroi1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem4Heroi1.FormattingEnabled = true;
+			this.cmbItem4Heroi1.Location = new System.Drawing.Point(40, 346);
+			this.cmbItem4Heroi1.Name = "cmbItem4Heroi1";
+			this.cmbItem4Heroi1.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem4Heroi1.TabIndex = 5;
+			// 
+			// cmbItem4Heroi2
+			// 
+			this.cmbItem4Heroi2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem4Heroi2.FormattingEnabled = true;
+			this.cmbItem4Heroi2.Location = new System.Drawing.Point(350, 346);
+			this.cmbItem4Heroi2.Name = "cmbItem4Heroi2";
+			this.cmbItem4Heroi2.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem4Heroi2.TabIndex = 10;
+			// 
+			// cmbItem5Heroi2
+			// 
+			this.cmbItem5Heroi2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem5Heroi2.FormattingEnabled = true;
+			this.cmbItem5Heroi2.Location = new System.Drawing.Point(350, 373);
+			this.cmbItem5Heroi2.Name = "cmbItem5Heroi2";
+			this.cmbItem5Heroi2.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem5Heroi2.TabIndex = 11;
+			// 
+			// cmbItem3Heroi2
+			// 
+			this.cmbItem3Heroi2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem3Heroi2.FormattingEnabled = true;
+			this.cmbItem3Heroi2.Location = new System.Drawing.Point(350, 319);
+			this.cmbItem3Heroi2.Name = "cmbItem3Heroi2";
+			this.cmbItem3Heroi2.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem3Heroi2.TabIndex = 8;
+			// 
+			// cmbItem6Heroi2
+			// 
+			this.cmbItem6Heroi2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem6Heroi2.FormattingEnabled = true;
+			this.cmbItem6Heroi2.Location = new System.Drawing.Point(350, 400);
+			this.cmbItem6Heroi2.Name = "cmbItem6Heroi2";
+			this.cmbItem6Heroi2.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem6Heroi2.TabIndex = 9;
+			// 
+			// cmbItem2Heroi2
+			// 
+			this.cmbItem2Heroi2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem2Heroi2.FormattingEnabled = true;
+			this.cmbItem2Heroi2.Location = new System.Drawing.Point(350, 292);
+			this.cmbItem2Heroi2.Name = "cmbItem2Heroi2";
+			this.cmbItem2Heroi2.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem2Heroi2.TabIndex = 7;
+			// 
+			// cmbItem1Heroi2
+			// 
+			this.cmbItem1Heroi2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbItem1Heroi2.FormattingEnabled = true;
+			this.cmbItem1Heroi2.Location = new System.Drawing.Point(350, 265);
+			this.cmbItem1Heroi2.Name = "cmbItem1Heroi2";
+			this.cmbItem1Heroi2.Size = new System.Drawing.Size(121, 21);
+			this.cmbItem1Heroi2.TabIndex = 6;
+			// 
+			// btnX1
+			// 
+			this.btnX1.Location = new System.Drawing.Point(217, 400);
+			this.btnX1.Name = "btnX1";
+			this.btnX1.Size = new System.Drawing.Size(75, 23);
+			this.btnX1.TabIndex = 12;
+			this.btnX1.Text = "X1";
+			this.btnX1.UseVisualStyleBackColor = true;
+			this.btnX1.Click += new System.EventHandler(this.btnX1_Click);
+			// 
+			// lblHeroi1
+			// 
+			this.lblHeroi1.AutoSize = true;
+			this.lblHeroi1.BackColor = System.Drawing.Color.Black;
+			this.lblHeroi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.lblHeroi1.ForeColor = System.Drawing.Color.White;
+			this.lblHeroi1.Location = new System.Drawing.Point(40, 185);
+			this.lblHeroi1.Name = "lblHeroi1";
+			this.lblHeroi1.Size = new System.Drawing.Size(56, 24);
+			this.lblHeroi1.TabIndex = 16;
+			this.lblHeroi1.Text = "Herói";
+			// 
+			// lblHeroi2
+			// 
+			this.lblHeroi2.AutoSize = true;
+			this.lblHeroi2.BackColor = System.Drawing.Color.Black;
+			this.lblHeroi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.lblHeroi2.ForeColor = System.Drawing.Color.White;
+			this.lblHeroi2.Location = new System.Drawing.Point(415, 185);
+			this.lblHeroi2.Name = "lblHeroi2";
+			this.lblHeroi2.Size = new System.Drawing.Size(56, 24);
+			this.lblHeroi2.TabIndex = 17;
+			this.lblHeroi2.Text = "Herói";
+			// 
+			// lblItens1
+			// 
+			this.lblItens1.AutoSize = true;
+			this.lblItens1.BackColor = System.Drawing.Color.Black;
+			this.lblItens1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.lblItens1.ForeColor = System.Drawing.Color.White;
+			this.lblItens1.Location = new System.Drawing.Point(40, 238);
+			this.lblItens1.Name = "lblItens1";
+			this.lblItens1.Size = new System.Drawing.Size(49, 24);
+			this.lblItens1.TabIndex = 18;
+			this.lblItens1.Text = "Itens";
+			// 
+			// lblItens2
+			// 
+			this.lblItens2.AutoSize = true;
+			this.lblItens2.BackColor = System.Drawing.Color.Black;
+			this.lblItens2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.lblItens2.ForeColor = System.Drawing.Color.White;
+			this.lblItens2.Location = new System.Drawing.Point(422, 238);
+			this.lblItens2.Name = "lblItens2";
+			this.lblItens2.Size = new System.Drawing.Size(49, 24);
+			this.lblItens2.TabIndex = 19;
+			this.lblItens2.Text = "Itens";
+			// 
+			// imgHeroi1
+			// 
+			this.imgHeroi1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.imgHeroi1.Location = new System.Drawing.Point(0, 0);
+			this.imgHeroi1.Margin = new System.Windows.Forms.Padding(0);
+			this.imgHeroi1.Name = "imgHeroi1";
+			this.imgHeroi1.Size = new System.Drawing.Size(255, 454);
+			this.imgHeroi1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.imgHeroi1.TabIndex = 13;
+			this.imgHeroi1.TabStop = false;
+			this.imgHeroi1.Visible = false;
+			// 
+			// imgHeroi2
+			// 
+			this.imgHeroi2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.imgHeroi2.Location = new System.Drawing.Point(255, 0);
+			this.imgHeroi2.Margin = new System.Windows.Forms.Padding(0);
+			this.imgHeroi2.Name = "imgHeroi2";
+			this.imgHeroi2.Size = new System.Drawing.Size(255, 454);
+			this.imgHeroi2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.imgHeroi2.TabIndex = 14;
+			this.imgHeroi2.TabStop = false;
+			this.imgHeroi2.Visible = false;
+			// 
+			// frmPrincipal
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::MlbbX1Calculator.Properties.Resources.fundo;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(510, 450);
+			this.Controls.Add(this.lblItens2);
+			this.Controls.Add(this.lblItens1);
+			this.Controls.Add(this.lblHeroi2);
+			this.Controls.Add(this.lblHeroi1);
+			this.Controls.Add(this.btnX1);
+			this.Controls.Add(this.cmbItem4Heroi2);
+			this.Controls.Add(this.cmbItem5Heroi2);
+			this.Controls.Add(this.cmbItem3Heroi2);
+			this.Controls.Add(this.cmbItem6Heroi2);
+			this.Controls.Add(this.cmbItem2Heroi2);
+			this.Controls.Add(this.cmbItem1Heroi2);
+			this.Controls.Add(this.cmbItem4Heroi1);
+			this.Controls.Add(this.cmbItem5Heroi1);
+			this.Controls.Add(this.cmbItem3Heroi1);
+			this.Controls.Add(this.cmbItem6Heroi1);
+			this.Controls.Add(this.cmbItem2Heroi1);
+			this.Controls.Add(this.cmbItem1Heroi1);
+			this.Controls.Add(this.cmbHeroi2);
+			this.Controls.Add(this.cmbHeroi1);
+			this.Controls.Add(this.imgHeroi1);
+			this.Controls.Add(this.imgHeroi2);
+			this.MaximumSize = new System.Drawing.Size(526, 489);
+			this.Name = "frmPrincipal";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "MLBB X1 Calculator";
+			((System.ComponentModel.ISupportInitialize)(this.imgHeroi1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgHeroi2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.ComboBox cmbHeroi2;
+		private System.Windows.Forms.ComboBox cmbItem1Heroi1;
+		private System.Windows.Forms.ComboBox cmbItem2Heroi1;
+		private System.Windows.Forms.ComboBox cmbItem6Heroi1;
+		private System.Windows.Forms.ComboBox cmbItem3Heroi1;
+		private System.Windows.Forms.ComboBox cmbItem5Heroi1;
+		private System.Windows.Forms.ComboBox cmbItem4Heroi1;
+		private System.Windows.Forms.ComboBox cmbItem4Heroi2;
+		private System.Windows.Forms.ComboBox cmbItem5Heroi2;
+		private System.Windows.Forms.ComboBox cmbItem3Heroi2;
+		private System.Windows.Forms.ComboBox cmbItem6Heroi2;
+		private System.Windows.Forms.ComboBox cmbItem2Heroi2;
+		private System.Windows.Forms.ComboBox cmbItem1Heroi2;
+		private System.Windows.Forms.Button btnX1;
+		private System.Windows.Forms.PictureBox imgHeroi1;
+		private System.Windows.Forms.PictureBox imgHeroi2;
+		private System.Windows.Forms.ComboBox cmbHeroi1;
+		private System.Windows.Forms.Label lblHeroi1;
+		private System.Windows.Forms.Label lblHeroi2;
+		private System.Windows.Forms.Label lblItens1;
+		private System.Windows.Forms.Label lblItens2;
+	}
+}
+
---
+++ MlbbX1Calculator/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace MlbbX1Calculator
+{
+	public partial class frmPrincipal : Form
+	{
+		Hashtable herois = new Hashtable();
+		Hashtable itens = new Hashtable();
+		public frmPrincipal()
+		{
+			InitializeComponent();
+
+			herois.Add("Layla", new Layla());
+			herois.Add("Zilong", new Zilong());
+			herois.Add("Terizla", new Terizla());
+			herois.Add("Dyrroth", new Dyrroth());
+			herois.Add("Silvana", new Silvana());
+			herois.Add("Rafaela", new Rafaela());
+			herois.Add("Gord", new Gord());
+			herois.Add("Estes", new Estes());
+			herois.Add("Harley", new Harley());
+			herois.Add("Angela", new Angela());
+			herois.Add("Kaja", new Kaja());
+			herois.Add("Thamuz", new Thamuz());
+			herois.Add("Guinevere", new Guinevere());
+			herois.Add("Lylia", new Lylia());
+			herois.Add("Hylos", new Hylos());
+			herois.Add("Wanwan", new Wanwan());
+			foreach (DictionaryEntry item in herois) {
+				cmbHeroi1.Items.Add(item.Key.ToString());
+				cmbHeroi2.Items.Add(item.Key.ToString());
+			}
+
+			itens.Add("", -1); //para campo nulo no combobox
+			itens.Add("Lâmina do desespero", new LaminaDoDesespero());
+			itens.Add("Voz do vento", new VozDoVento());
+			foreach (DictionaryEntry item in itens) {
+				cmbItem1Heroi1.Items.Add(item.Key.ToString());
+				cmbItem2Heroi1.Items.Add(item.Key.ToString());
+				cmbItem3Heroi1.Items.Add(item.Key.ToString());
+				cmbItem4Heroi1.Items.Add(item.Key.ToString());
+				cmbItem5Heroi1.Items.Add(item.Key.ToString());
+				cmbItem6Heroi1.Items.Add(item.Key.ToString());
+				cmbItem1Heroi2.Items.Add(item.Key.ToString());
+				cmbItem2Heroi2.Items.Add(item.Key.ToString());
+				cmbItem3Heroi2.Items.Add(item.Key.ToString());
+				cmbItem4Heroi2.Items.Add(item.Key.ToString());
+				cmbItem5Heroi2.Items.Add(item.Key.ToString());
+				cmbItem6Heroi2.Items.Add(item.Key.ToString());
+			}
+		}
+
+		private void atribuiItens(Heroi hero1, Heroi hero2) {
+			Item item1h1, item2h1, item3h1, item4h1, item5h1, item6h1;
+			Item item1h2, item2h2, item3h2, item4h2, item5h2, item6h2;
+
+			if (cmbItem1Heroi1.SelectedIndex != -1 && cmbItem1Heroi1.SelectedItem.ToString() != "") {
+				item1h1 = (Item)itens[cmbItem1Heroi1.SelectedItem.ToString()];
+				item1h1.add(hero1);
+			}
+			if (cmbItem2Heroi1.SelectedIndex != -1 && cmbItem2Heroi1.SelectedItem.ToString() != "") {
+				item2h1 = (Item)itens[cmbItem2Heroi1.SelectedItem.ToString()];
+				item2h1.add(hero1);
+			}
+			if (cmbItem3Heroi1.SelectedIndex != -1 && cmbItem3Heroi1.SelectedItem.ToString() != "") {
+				item3h1 = (Item)itens[cmbItem3Heroi1.SelectedItem.ToString()];
+				item3h1.add(hero1);
+			}
+			if (cmbItem4Heroi1.SelectedIndex != -1 && cmbItem4Heroi1.SelectedItem.ToString() != "") {
+				item4h1 = (Item)itens[cmbItem4Heroi1.SelectedItem.ToString()];
+				item4h1.add(hero1);
+			}
+			if (cmbItem5Heroi1.SelectedIndex != -1 && cmbItem5Heroi1.SelectedItem.ToString() != "") {
+				item5h1 = (Item)itens[cmbItem5Heroi1.SelectedItem.ToString()];
+				item5h1.add(hero1);
+			}
+			if (cmbItem6Heroi1.SelectedIndex != -1 && cmbItem6Heroi1.SelectedItem.ToString() != "") {
+				item6h1 = (Item)itens[cmbItem6Heroi1.SelectedItem.ToString()];
+				item6h1.add(hero1);
+			}
+			if (cmbItem1Heroi2.SelectedIndex != -1 && cmbItem1Heroi2.SelectedItem.ToString() != "") {
+				item1h2 = (Item)itens[cmbItem1Heroi2.SelectedItem.ToString()];
+				item1h2.add(hero2);
+			}
+			if (cmbItem2Heroi2.SelectedIndex != -1 && cmbItem2Heroi2.SelectedItem.ToString() != "") {
+				item2h2 = (Item)itens[cmbItem2Heroi2.SelectedItem.ToString()];
+				item2h2.add(hero2);
+			}
+			if (cmbItem3Heroi2.SelectedIndex != -1 && cmbItem3Heroi2.SelectedItem.ToString() != "") {
+				item3h2 = (Item)itens[cmbItem3Heroi2.SelectedItem.ToString()];
+				item3h2.add(hero2);
+			}
+			if (cmbItem4Heroi2.SelectedIndex != -1 && cmbItem4Heroi2.SelectedItem.ToString() != "") {
+				item4h2 = (Item)itens[cmbItem4Heroi2.SelectedItem.ToString()];
+				item4h2.add(hero2);
+			}
+			if (cmbItem5Heroi2.SelectedIndex != -1 && cmbItem5Heroi2.SelectedItem.ToString() != "") {
+				item5h2 = (Item)itens[cmbItem5Heroi2.SelectedItem.ToString()];
+				item5h2.add(hero2);
+			}
+			if (cmbItem6Heroi2.SelectedIndex != -1 && cmbItem6Heroi2.SelectedItem.ToString() != "") {
+				item6h2 = (Item)itens[cmbItem6Heroi2.SelectedItem.ToString()];
+				item6h2.add(hero2);
+			}
+		}
+
+		private void btnX1_Click(object sender, EventArgs e) {
+			Heroi hero1, hero2;
+			int dano = 0;
+			int danoTotal1 = 0, danoTotal2 = 0;
+			string selecao1;
+			string selecao2;
+			Type class1, class2;
+
+			cmbHeroi1.Focus(); //apenas para remover o foco do botão
+
+			if (cmbHeroi1.SelectedItem == null || cmbHeroi2.SelectedItem == null) {
+				MessageBox.Show("Selecione os heróis", "Dados incompletos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+			else {
+				selecao1 = cmbHeroi1.SelectedItem.ToString();
+				selecao2 = cmbHeroi2.SelectedItem.ToString();
+				class1 = herois[selecao1].GetType();
+				hero1 = (Heroi)Activator.CreateInstance(class1);
+				class2 = herois[selecao2].GetType();
+				hero2 = (Heroi)Activator.CreateInstance(class2);
+				this.atribuiItens(hero1, hero2);
+
+				while (hero1.Hp != 0 && hero2.Hp != 0) {
+					dano = hero1.atacar();
+					danoTotal1 += dano;
+					if (hero1.DanoReal == true)
+						hero2.recebeDano(dano, true);
+					else
+						hero2.recebeDano(dano);
+
+					dano = hero2.atacar();
+					danoTotal2 += dano;
+					if (hero2.DanoReal == true)
+						hero1.recebeDano(dano, true);
+					else
+						hero1.recebeDano(dano);
+				}
+
+				if (hero1.Hp == 0) {
+					MessageBox.Show(
+						hero2.Nome + " ganha\n" +
+						"Dano total " + hero1.Nome + " = " + danoTotal1 + "\n" +
+						"Dano total " + hero2.Nome + " = " + danoTotal2,
+						hero1.Nome + " x " + hero2.Nome);
+				}
+				else if (hero2.Hp == 0) {
+					MessageBox.Show(
+						hero1.Nome + " ganha\n" +
+						"Dano total " + hero1.Nome + " = " + danoTotal1 + "\n" +
+						"Dano total " + hero2.Nome + " = " + danoTotal2,
+						hero1.Nome + " x " + hero2.Nome);
+				}
+			}
+		}
+
+		private void cmbHeroi1_SelectedIndexChanged(object sender, EventArgs e) {
+			string selecao = cmbHeroi1.SelectedItem.ToString();
+
+			imgHeroi1.Visible = true;
+			Heroi heroiSelecionado = (Heroi)herois[selecao];
+			imgHeroi1.Image = heroiSelecionado.Imagem;
+
+			//Tentei deixar o fundo transparente mas não consegui
+			/*
+			lblHeroi1.ForeColor = System.Drawing.Color.Black;
+			lblItens1.ForeColor = System.Drawing.Color.Black;
+			lblHeroi1.Parent = imgHeroi2;
+			lblHeroi1.BackColor = Color.Transparent;
+			lblItens1.Parent = imgHeroi2;
+			lblItens1.BackColor = Color.Transparent; */
+			lblHeroi1.Visible = false;
+			lblItens1.Visible = false;
+
+		}
+
+		private void cmbHeroi2_SelectedIndexChanged(object sender, EventArgs e) {
+			string selecao = cmbHeroi2.SelectedItem.ToString();
+
+			imgHeroi2.Visible = true;
+			Heroi heroiSelecionado = (Heroi)herois[selecao];
+			imgHeroi2.Image = heroiSelecionado.Imagem;
+
+			//Tentei deixar o fundo transparente mas não consegui
+			/*lblHeroi2.ForeColor = System.Drawing.Color.Black;
+			lblItens2.ForeColor = System.Drawing.Color.Black;
+			lblHeroi2.Parent = imgHeroi2;
+			lblHeroi2.BackColor = Color.Transparent;
+			lblItens2.Parent = imgHeroi2;
+			lblItens2.BackColor = Color.Transparent;*/
+			lblItens2.Visible = false;
+			lblHeroi2.Visible = false;
+
+		}
+
+		private void cmbItem1Heroi1_SelectedIndexChanged(object sender, EventArgs e) {
+			string selecao = cmbItem1Heroi1.SelectedIndex.ToString();
+			if (selecao == "") {
+				cmbItem1Heroi1.ResetText();
+				cmbItem1Heroi1.SelectedItem = null;
+			}
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e) {
+
+		}
+	}
+}
